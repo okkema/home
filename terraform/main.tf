93cc76016365392c1508e60c443115685028303f
Benjamin Okkema <ben@okkema.org>
2023-10-29 21:54:48 -0500

    feat: terraform

---
+++ terraform/main.tf
@@ -0,0 +1,61 @@
+locals {
+  secrets = {
+    "TF_API_TOKEN" : var.TF_API_TOKEN,
+    "ACTIONS_GITHUB_TOKEN" : var.ACTIONS_GITHUB_TOKEN
+  }
+}
+
+module "secrets" {
+  for_each = local.secrets
+
+  source  = "app.terraform.io/okkema/secret/github"
+  version = "~> 0.2"
+
+  repository = var.github_repository
+  key        = each.key
+  value      = each.value
+}
+
+module "page" {
+  source     = "app.terraform.io/okkema/page/cloudflare"
+  version    = "~> 0.2"
+  depends_on = [module.application, module.bucket]
+
+  account_id = var.cloudflare_account_id
+  zone_id    = var.cloudflare_zone_id
+  name       = var.github_repository
+  repo_name  = var.github_repository
+  production_buckets = {
+    BUCKET = module.bucket.id
+  }
+  production_secrets = {
+    CLOUDFLARE_ACCESS_DOMAIN = var.CLOUDFLARE_ACCESS_DOMAIN
+    CLOUDFLARE_ACCESS_AUD    = module.application.aud
+    SENTRY_DSN               = var.SENTRY_DSN
+  }
+}
+
+module "application" {
+  source     = "app.terraform.io/okkema/application/cloudflare"
+  version    = "~> 0.4"
+  depends_on = [module.team]
+
+  zone_id      = var.cloudflare_zone_id
+  name         = var.github_repository
+  github_teams = [var.github_repository]
+}
+
+module "team" {
+  source  = "app.terraform.io/okkema/team/github"
+  version = "~> 0.1"
+
+  name = var.github_repository
+}
+
+module "bucket" {
+  source  = "app.terraform.io/okkema/bucket/cloudflare"
+  version = "~> 1.0"
+
+  account_id = var.cloudflare_account_id
+  name       = var.github_repository
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "cloudflare_account_id" {}
+variable "cloudflare_zone_id" {}
+variable "github_repository" {}
+
+# GitHub Actions Secrets
+variable "TF_API_TOKEN" {
+  sensitive = true
+}
+variable "ACTIONS_GITHUB_TOKEN" {
+  sensitive = true
+}
+
+# Environment Variables
+variable "SENTRY_DSN" {
+  sensitive = true
+}
+variable "CLOUDFLARE_ACCESS_DOMAIN" {}

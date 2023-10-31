81f968fee8f80701bd46fadbdabef5a6d5464dd3
Benjamin Okkema <ben@okkema.org>
2023-10-30 23:51:23 -0500

    feat: semantic ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig, passthroughImageService } from "astro/config"
-import cloudflare from "@astrojs/cloudflare"
+import { defineConfig, passthroughImageService } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
 
 export default defineConfig({
   output: "server",
-  adapter: cloudflare({ mode: "directory" }),
+  adapter: cloudflare({
+    mode: "directory"
+  }),
   image: {
-    service: passthroughImageService(),
+    service: passthroughImageService()
   },
-})
+  integrations: [react()]
+});

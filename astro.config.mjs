6637909a61dcbd9d0b4fffe70e56faa180177f0e
Benjamin Okkema <ben@okkema.org>
2023-11-18 00:07:09 -0600

    feat: astro sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,24 @@
-import { defineConfig, passthroughImageService } from "astro/config"
-import cloudflare from "@astrojs/cloudflare"
-import react from "@astrojs/react"
+import { defineConfig, passthroughImageService } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
+import sentry from "@sentry/astro";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare({
     mode: "directory",
     routes: {
-      strategy: "exclude",
-    },
+      strategy: "exclude"
+    }
   }),
   image: {
     service: passthroughImageService()
   },
-  integrations: [react()]
+  integrations: [
+    react(), 
+    sentry({
+      dsn: process.env.SENTRY_DSN,
+    }),
+  ]
 });
